720329f885c94f96bc1f10482f9b7969534d0ef2
1053520131@qq.com <1053520131@qq.com>
2016-03-07 12:39:53 +0800

    new

---
+++ uii.php
@@ -0,0 +1,13 @@
+
+<?php 
+echo <<< EOT 
+<table width=80% border="2" cellpadding="3" cellspacing="0" bordercolor="#808080"> 
+<tr bgcolor="#84A9E1"> 
+<td align="center">ClassID</td> 
+<td align="center">stuno</td> 
+<td align="center">学生姓名</td> 
+<td align="center">家长姓名</td> 
+<td align="center">家长手机号</td> 
+</tr> 
+EOT; 
+?> 
